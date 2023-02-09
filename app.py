320fe9d678e6250d3a9e53b3e352caaf26ef68b4
melitina915 <melitina915@gmail.com>
2023-02-09 10:32:09 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+
+from dash import Dash, dcc, html
+
+from callbacks import get_callbacks
+from utils import MBTI
+
+app = Dash(__name__)
+
+
+def user_controls():
+    return html.Div(
+        [   
+            html.Label("이름"),
+            dcc.Input(placeholder="아무개", type="text", id="name-input"),
+            html.Label("나이"),
+            dcc.Input(placeholder="20", type="number", id="age-input"),
+            html.Label("MBTI"),
+            dcc.Dropdown(options=MBTI, id="mbti-input"),
+            html.Label("아바타 재생성"),
+            html.Button("재생성", id="avatar-button", style={"font-size": "16px"}),
+            # dcc.Input(), 
+            # dcc.Input(), 
+            # dcc.Dropdown(),
+            # html.Button(),
+        ],
+        className="three columns div-user-controls",
+    )
+
+
+def profile():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.H2("Profile", style={"font-size": "36px"}),
+                    html.Div([
+                        html.H2("이름", id="name"),
+                        html.H2("나이", id="age"),
+                    ]),
+                    html.Img(id="avatar")
+                ]
+            ),
+            html.H2("MBTI", style={"font-size": "12px"}),
+            dcc.Graph(id="mbti")
+        ],
+        className="nine columns bg-grey",
+        style={
+            "display": "flex",
+            "flex-direction": "column",
+            "flex-grow": 1,
+        },
+    )
+
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className="row",
+            children=[
+                user_controls(),
+                profile(),
+            ],
+        )
+    ]
+)
+
+get_callbacks(app)
+
+# Remove all svg files is assets folder
+for file in os.listdir("assets"):
+    if file.endswith(".svg"):
+        os.remove(os.path.join("assets", file))
+
+
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
+
+if __name__ == "__main__":
+    app.run_server()
